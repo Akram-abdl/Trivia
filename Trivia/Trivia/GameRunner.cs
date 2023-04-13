205e9aa1b7f75af0d09bbe4dac2b280f55105651
joris <joris.rouziere@gmail.cm>
2023-04-13 09:50:45 +0200

    changement de la condition pour sortir de prison

--- Trivia/Trivia/GameRunner.cs
+++ Trivia/Trivia/GameRunner.cs
@@ -7,6 +7,7 @@ namespace Trivia
     {
         private static bool _notAWinner;
         private readonly IConsole console;
+        private Random rand;
 
         public static void Main(string[] args)
         {
@@ -16,11 +17,14 @@ namespace Trivia
         private GameRunner()
         {
             console = new SystemConsole();
+            rand = new Random();
+            console.WriteLine(rand.Next(1, 1).ToString());
         }
 
         public GameRunner(IConsole console)
         {
             this.console = console;
+            rand = new Random();
         }
 
         // play a game
@@ -30,7 +34,7 @@ namespace Trivia
             string userPreference = Console.ReadLine();
             bool replaceRockWithTechno = userPreference.ToLower() == "yes";
 
-            var aGame = new Game(console, replaceRockWithTechno, goldCoinsToWin);
+            var aGame = new Game(console, rand, replaceRockWithTechno, goldCoinsToWin);
 
             Game(aGame, players);
         }
@@ -38,7 +42,7 @@ namespace Trivia
         // play a game test
         public void PlayAGameTest(List<String> players, bool rockTechno, int goldCoinsToWin = 6)
         {
-            var aGame = new Game(console, rockTechno, goldCoinsToWin);
+            var aGame = new Game(console, rand, rockTechno, goldCoinsToWin);
 
             Game(aGame, players);
         }
@@ -51,8 +55,6 @@ namespace Trivia
                 {
                     aGame.Add(player);
                 }
-
-                var rand = new Random();
 
                 do
                 {
